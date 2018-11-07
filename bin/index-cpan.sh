aa18a51961946f2e0f8dda13005680c655501cff
Tom Molesworth <tom@binary.com>
2018-11-07 15:19:40 +0800

    Avoid partial mirror - we want just the `cpan` repo

--- bin/index-cpan.sh
+++ bin/index-cpan.sh
@@ -2,7 +2,7 @@
 
 ./bin/run bin/metacpan mapping --delete
 
-/bin/partial-cpan-mirror.sh
+# /bin/partial-cpan-mirror.sh
 
 ./bin/run bin/metacpan release /CPAN/authors/id/
 ./bin/run bin/metacpan latest
